6a42e4fee0e7a4638963216a2fe1a59b9ac8c6a1
Lucas Magalhaes <lucasdecassia9@hotmail.com>
2024-08-30 17:48:11 -0300

    new teste for failed.

--- tests/browser.spec.ts
+++ tests/browser.spec.ts
@@ -31,4 +31,18 @@ test('fill in the fields and click the Start challenge button', async ({ page })
 
 });
 
+test('Click the start button and wait for the error message to appear', async ({ page }) => {
+
+  await page.goto('http://localhost:5173/');
+
+  await page.fill('input[type="text"]', 'Lucas');
+  await page.fill('input[type="tel"]', '123');
+  await page.fill('input[type="email"]', 'lucasdecassia9@hotmail.com');
+
+  await page.click('button:has-text("Start Challenge")');
+
+  await page.waitForTimeout(15000);
+
+  await expect(page.locator('text=Challenge failed!')).toBeVisible();
+});
 
